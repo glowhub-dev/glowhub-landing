6fba6c84995c2535fd609a3ffad79fdef434117b
Manuel Carrillo Almoguera <manucaralmo@gmail.com>
2021-05-07 21:03:52 +0200

    pricing page

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,10 +5,11 @@ const Nav = () => {
   return (
     <>
       <Link href="/" className="navLink">Home</Link>
-      <Link href="/" className="navLink">Overview</Link>
-      <Link href="/" className="navLink">Pricing</Link>
-      <Link href="/" className="navLink">Faq</Link>
-      <Link href="https://app.glowhub.dev" className="navLink">Login</Link>
+      <Link href="/" className="navLink">Cookies</Link>
+      <Link href="/" className="navLink">Analytics</Link>
+      <Link href="/" className="navLink">Feedback</Link>
+      <Link href="/pricing" className="navLink">Pricing</Link>
+      <a href="https://app.glowhub.dev" className="navLink">Login</a>
       <a href="https://app.glowhub.dev/register" className="navLink">Register</a>
     </>
   )
